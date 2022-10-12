2e4ed6e6cd47dd62f7aab8480ac98efc4009cf03
void_zaid <renediaz725@gmail.com>
2022-10-11 22:14:48 -0500

    agregando la funcionalidad del menu responsive

--- js/menu_responsive.js
+++ js/menu_responsive.js
@@ -1,4 +1,15 @@
 let boton_menu = document.querySelector("#button-menu");
 let anchors = document.querySelector("#anchors-proyects");
+let bgMenu = document.getElementById("dark-bg-menu");
 
-boton_menu.onclick.addClass
+boton_menu.addEventListener('click', showMenu);
+bgMenu.addEventListener('click', hideMenu);
+
+function showMenu(){
+    anchors.style.right = "0px";
+    bgMenu.style.display = "block";
+}
+function hideMenu(){
+    anchors.style.right = "-400px";
+    bgMenu.style.display = "none"
+}
